80505ca3fd99edf450478aabe986e51fbb4af416
jfearnow <jfearnow79@gmail.com>
2019-02-28 09:08:17 -0500

    added comments to the program

--- puzzle.py
+++ puzzle.py
@@ -1,16 +1,19 @@
 import pygame, sys, random
 from pygame.locals import *
-
+'''
+for more detailed information about this game https://inventwithpython.com/makinggames.pdf goto chapter 4 pg 77-107.
+'''
 # Create the constants (go ahead and experiment with different values)
 BOARDWIDTH = 4  # number of columns in the board
 BOARDHEIGHT = 4 # number of rows in the board
-TILESIZE = 80
-WINDOWWIDTH = 640
-WINDOWHEIGHT = 480
-FPS = 60
+TILESIZE = 80 # the tile size
+WINDOWWIDTH = 640 # width of game window
+WINDOWHEIGHT = 480 # height of game window
+FPS = 60 #frames per second for game tiles
 BLANK = None
 
 #                 R    G    B
+# color options to be used within the game design if wanted:
 BLACK =         (  0,   0,   0)
 WHITE =         (255, 255, 255)
 BRIGHTBLUE =    (  0,  50, 255)
@@ -20,19 +23,19 @@ DARKGREEN =     ( 34, 139,  34)
 DARKYELLOW =    (204, 204,   0)
 TOMATORED =     (255,  99,  71)
 
-BGCOLOR = DARKTURQUOISE
-TILECOLOR = DARKGREEN
-TEXTCOLOR = TOMATORED
-BORDERCOLOR = DARKYELLOW
-BASICFONTSIZE = 20
+BGCOLOR = DARKTURQUOISE # background color
+TILECOLOR = DARKGREEN # main tile color
+TEXTCOLOR = TOMATORED # color of the text in game
+BORDERCOLOR = DARKYELLOW # color for the border of the board
+BASICFONTSIZE = 20 # font size in game
 
-BUTTONCOLOR = WHITE
-BUTTONTEXTCOLOR = BLACK
-MESSAGECOLOR = WHITE
+MESSAGECOLOR = WHITE # color of pop up messages
 
+# giving math operations to the overall size of the game vertical and horizontal values
 XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
 
+# telling how to move the tiles to blank spot later on in the program.
 UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
@@ -40,31 +43,33 @@ RIGHT = 'right'
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
+# global you are able to change the value of the variables within the function.
 
-    pygame.init()
-    FPSCLOCK = pygame.time.Clock()
+    pygame.init() # game initiation 
+    FPSCLOCK = pygame.time.Clock() # clock to track time of game
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-    pygame.display.set_caption('Puzzle Game')
-    BASICFONT = pygame.font.SysFont('freesansbold.ttf', BASICFONTSIZE)
+    pygame.display.set_caption('Puzzle Game') #displays the title of the game
+    BASICFONT = pygame.font.SysFont('freesansbold.ttf', BASICFONTSIZE) #font of the of the "display title for game"
 
     # Store the option buttons and their rectangles in OPTIONS.
-    RESET_SURF, RESET_RECT = makeText('Reset',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90)
+    # this is gives you the text and tile colors as well as dimensions of the button size
+    RESET_SURF, RESET_RECT = makeText('Reset',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 90) 
     NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
     SOLVE_SURF, SOLVE_RECT = makeText('Solve',    TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
 
-    mainBoard, solutionSeq = generateNewPuzzle(80)
+    mainBoard, solutionSeq = generateNewPuzzle(80) # mainboard is generating a new puzzle
     SOLVEDBOARD = getStartingBoard() # a solved board is the same as the board in a start state.
     allMoves = [] # list of moves made from the solved configuration
 
     while True: # main game loop
         slideTo = None # the direction, if any, a tile should slide
         msg = 'Click tile or press arrow keys to slide.' # contains the message to show in the upper left corner.
-        if mainBoard == SOLVEDBOARD:
-            msg = "You're a Genius!"
+        if mainBoard == SOLVEDBOARD: # once the game is solved returns the message "You're a Genius"
+            msg = "You're a Genius!" # message to show once you solve the game
 
-        drawBoard(mainBoard, msg)
+        drawBoard(mainBoard, msg) # mainboard messages 
 
-        checkForQuit()
+        checkForQuit() # for loop checking to see how to respond to reset, new or solve if clicked.
         for event in pygame.event.get(): 
             if event.type == UP:
                 spotx, spoty = getSpotClicked(mainBoard, event.pos[0], event.pos[1])
@@ -82,7 +87,7 @@ def main():
                         allMoves = []
                 else:
                     # check if the clicked tile was next to the blank spot
-
+                    # this is the where the program decides what tile to move to an empty spot.
                     blankx, blanky = getBlankPosition(mainBoard)
                     if spotx == blankx + 1 and spoty == blanky:
                         slideTo = LEFT
@@ -94,6 +99,7 @@ def main():
                         slideTo = DOWN
 
             elif event.type == KEYUP:
+                #keyboard constant commands and represents which key to hit to move the tile and what direction it is suppose to move.
                 # check if the user pressed a key to slide a tile
                 if event.key in (K_LEFT, K_a) and isValidMove(mainBoard, LEFT):
                     slideTo = LEFT
@@ -108,14 +114,14 @@ def main():
             makeMove(mainBoard, slideTo)
             allMoves.append(slideTo) # record the slide
         pygame.display.update()
-        FPSCLOCK.tick(FPS)
+        FPSCLOCK.tick(FPS) # frames per second ticker
 
-
+# this is terminating the game and exit once it is terminated.
 def terminate():
     sys.exit()
 
-
-def checkForQuit():
+# you can hit the esc button to exit and terminate the game
+def checkForQuit(): 
     for event in pygame.event.get(QUIT): 
         terminate() 
     for event in pygame.event.get(KEYUP): 
@@ -123,7 +129,7 @@ def checkForQuit():
             terminate() 
         pygame.event.post(event) 
 
-
+# this is determining the order of tiles when game is started and also gives you the x and y dimensions of the board.
 def getStartingBoard():
     # Return a board data structure with tiles in the solved state.
     # For example, if BOARDWIDTH and BOARDHEIGHT are both 3, this function
@@ -141,9 +147,8 @@ def getStartingBoard():
     board[BOARDWIDTH-1][BOARDHEIGHT-1] = BLANK
     return board
 
-
+ # Return the x and y of board coordinates of the blank space.
 def getBlankPosition(board):
-    # Return the x and y of board coordinates of the blank space.
     for x in range(BOARDWIDTH):
         for y in range(BOARDHEIGHT):
             if board[x][y] == BLANK:
@@ -153,7 +158,7 @@ def getBlankPosition(board):
 def makeMove(board, move):
     # This function does not check if the move is valid.
     blankx, blanky = getBlankPosition(board)
-
+# this tells the program in which direction to move the tile and whether or not you can move in the given direction
     if move == UP:
         board[blankx][blanky], board[blankx][blanky + 1] = board[blankx][blanky + 1], board[blankx][blanky]
     elif move == DOWN:
@@ -163,7 +168,7 @@ def makeMove(board, move):
     elif move == RIGHT:
         board[blankx][blanky], board[blankx - 1][blanky] = board[blankx - 1][blanky], board[blankx][blanky]
 
-
+# this is validating that the move you choose will work or not.
 def isValidMove(board, move):
     blankx, blanky = getBlankPosition(board)
     return (move == UP and blanky != len(board[0]) - 1) or \
@@ -171,12 +176,12 @@ def isValidMove(board, move):
            (move == LEFT and blankx != len(board) - 1) or \
            (move == RIGHT and blankx != 0)
 
-
+# 
 def getRandomMove(board, lastMove=None):
     # start with a full list of all four moves
     validMoves = [UP, DOWN, LEFT, RIGHT]
 
-    # remove moves from the list as they are disqualified
+    # remove moves from the list as they are disqualified lines 183-190
     if lastMove == UP or not isValidMove(board, DOWN):
         validMoves.remove(DOWN)
     if lastMove == DOWN or not isValidMove(board, UP):
@@ -189,58 +194,65 @@ def getRandomMove(board, lastMove=None):
     # return a random move from the list of remaining moves
     return random.choice(validMoves)
 
-
+# tells the program in what direction to move the tiles and make it clean looking.
 def getLeftTopOfTile(tileX, tileY):
     left = XMARGIN + (tileX * TILESIZE) + (tileX - 1)
     top = YMARGIN + (tileY * TILESIZE) + (tileY - 1)
     return (left, top)
 
-
+# TODO: this is where i left off on commenting the lines out.
 def getSpotClicked(board, x, y):
     # from the x & y pixel coordinates, get the x & y board coordinates
-    for tileX in range(len(board)):
-        for tileY in range(len(board[0])):
-            left, top = getLeftTopOfTile(tileX, tileY)
-            tileRect = pygame.Rect(left, top, TILESIZE, TILESIZE)
+    for tileX in range(len(board)): # giving the range of the x axis of the tile
+        for tileY in range(len(board[0])): # giving the range of the y axis of the tile
+            left, top = getLeftTopOfTile(tileX, tileY) # tells you where the tile is moving
+            tileRect = pygame.Rect(left, top, TILESIZE, TILESIZE) 
             if tileRect.collidepoint(x, y):
-                return (tileX, tileY)
+                return (tileX, tileY) # returns the size of the tile if everything checks out
     return (None, None)
 
 
 def drawTile(tilex, tiley, number, adjx=0, adjy=0):
     # draw a tile at board coordinates tilex and tiley, optionally a few
     # pixels over (determined by adjx and adjy)
+    # take the display surface and draw it onto the screen and position it referencing x, y positions. also renders the font style and color
     left, top = getLeftTopOfTile(tilex, tiley)
     pygame.draw.rect(DISPLAYSURF, TILECOLOR, (left + adjx, top + adjy, TILESIZE, TILESIZE))
     textSurf = BASICFONT.render(str(number), True, TEXTCOLOR)
     textRect = textSurf.get_rect()
     textRect.center = left + int(TILESIZE / 2) + adjx, top + int(TILESIZE / 2) + adjy
-    DISPLAYSURF.blit(textSurf, textRect)
+    DISPLAYSURF.blit(textSurf, textRect) # this is taking in a covering over another area with tons of pixels to make the display surface.
 
-
+# this is giving you the color of text, background color and location of the message
 def makeText(text, color, bgcolor, top, left):
     # create the Surface and Rect objects for some text.
-    textSurf = BASICFONT.render(text, True, color, bgcolor)
-    textRect = textSurf.get_rect()
+    textSurf = BASICFONT.render(text, True, color, bgcolor) # this is giving the color, font of text
+    textRect = textSurf.get_rect() # this is aligning the text characters and making a rectangle
     textRect.topleft = (top, left)
-    return (textSurf, textRect)
+    return (textSurf, textRect) # this is returning the textSurf and textRect is an imaginary rectangle for text characters.
 
 
 def drawBoard(board, message):
-    DISPLAYSURF.fill(BGCOLOR)
+    DISPLAYSURF.fill(BGCOLOR) # color fill of the display surface.
     if message:
         textSurf, textRect = makeText(message, MESSAGECOLOR, BGCOLOR, 5, 5)
-        DISPLAYSURF.blit(textSurf, textRect)
+        DISPLAYSURF.blit(textSurf, textRect) # this is giving display surface of the buttons
 
+# this is taking the tiles at x and y axis to make each individual tile for the gameboard given the parameters in "board" @ line 144.
     for tilex in range(len(board)):
         for tiley in range(len(board[0])):
             if board[tilex][tiley]:
                 drawTile(tilex, tiley, board[tilex][tiley])
 
+#draw a rectangle shape
+# rect(Surface, color, Rect, width=0) -> Rect
+# Draws a rectangular shape on the Surface. The given Rect is the area of the rectangle. 
+# The width argument is the thickness to draw the outer edge. 
+# If width is zero then the rectangle will be filled.
     left, top = getLeftTopOfTile(0, 0)
     width = BOARDWIDTH * TILESIZE
     height = BOARDHEIGHT * TILESIZE
-    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4)
+    pygame.draw.rect(DISPLAYSURF, BORDERCOLOR, (left - 5, top - 5, width + 11, height + 11), 4) # drawing a rectangle for the menu buttons
 
     DISPLAYSURF.blit(RESET_SURF, RESET_RECT)
     DISPLAYSURF.blit(NEW_SURF, NEW_RECT)
@@ -249,7 +261,7 @@ def drawBoard(board, message):
 
 def slideAnimation(board, direction, message, animationSpeed):
     # Note: This function does not check if the move is valid.
-
+    # this is giving you the animation of the tiles when moving during the game. and telling it to move 1 space per click in any given direction.
     blankx, blanky = getBlankPosition(board)
     if direction == UP:
         movex = blankx
@@ -264,12 +276,12 @@ def slideAnimation(board, direction, message, animationSpeed):
         movex = blankx - 1
         movey = blanky
 
-    # prepare the base surface
+    # prepare the base surface/or foundation of the game
     drawBoard(board, message)
     baseSurf = DISPLAYSURF.copy()
-    # draw a blank space over the moving tile on the baseSurf Surface.
+    # draw a blank space over the moving tile on the baseSurf Surface. gives you the background color and size of tile is what you see after a tile has been moved.
     moveLeft, moveTop = getLeftTopOfTile(movex, movey)
-    pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft, moveTop, TILESIZE, TILESIZE))
+    pygame.draw.rect(baseSurf, BGCOLOR, (moveLeft, moveTop, TILESIZE, TILESIZE)) # after tile is moved the empty space takes on the background color, and indicates an empty space.
 
     for i in range(0, TILESIZE, animationSpeed):
         # animate the tile sliding over
@@ -291,6 +303,8 @@ def slideAnimation(board, direction, message, animationSpeed):
 def generateNewPuzzle(numSlides):
     # From a starting configuration, make numSlides number of moves (and
     # animate these moves).
+    # this gives a new sequence of tiles when starting a new game. when you reach the last move it will return None.
+    # it also gives the speed to generate the new game. 
     sequence = []
     board = getStartingBoard()
     drawBoard(board, '')
